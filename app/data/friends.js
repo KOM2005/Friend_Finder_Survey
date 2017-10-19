1f793220d81241a6d879918fd1a069f1c32d75dc
KOM2005 <oleh_kmyta@comcast.net>
2017-10-19 14:21:21 -0600

    working around increased #questions

---
+++ app/data/friends.js
@@ -0,0 +1,120 @@
+let friends = [
+{
+  "name":"Megan Fox",
+  "photo":"https://img.buzzfeed.com/buzzfeed-static/static/2015-08/14/11/campaign_images/webdr03/18-celebrity-morph-combinations-that-are-stunning-2-27971-1439566374-13_dblbig.jpg",
+  "scores":[
+      5,
+      1,
+      4,
+      4,
+      3,
+      1,
+      2,
+      4,
+      5,
+      3
+    ] 
+},
+
+{
+  "name": "KOM",
+  "photo": "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAA0oAAAAJDFmODJkY2QxLWUzMzgtNDA1Yy1iYTJmLTE3OWVhNzg3N2ZiYQ.jpg",
+  "scores": [
+    1,
+    2,
+    4,
+    5,
+    3,
+    5,
+    1,
+    4,
+    4,
+    3
+  ]
+  },
+  
+{
+  "name":"Angelina Jolie",
+  "photo":"http://cdn.playbuzz.com/cdn/11572c25-3aa8-41c5-8fef-795de51e4346/ee72093c-3c01-433a-8d25-701cca06c975.jpg",
+  "scores":[
+      3,
+      2,
+      5,
+      1,
+      2,
+      5,
+      1,
+      4,
+      4,
+      3
+    ]
+},
+{
+  "name":"Arnold Shwarzeneger",
+  "photo":"https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/A._Schwarzenegger.jpg/220px-A._Schwarzenegger.jpg",
+  "scores":[
+      2,
+      3,
+      4,
+      4,
+      3,
+      3,
+      2,
+      5,
+      1,
+      2
+    ]
+},
+{
+  "name":"Leonardo Di Caprio",
+  "photo":"https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Leonardo_DiCaprio_visited_Goddard_Saturday_to_discuss_Earth_science_with_Piers_Sellers_%2826105091624%29_cropped.jpg/220px-Leonardo_DiCaprio_visited_Goddard_Saturday_to_discuss_Earth_science_with_Piers_Sellers_%2826105091624%29_cropped.jpg",
+  "scores":[
+      5,
+      5,
+      5,
+      5,
+      4,
+      2,
+      3,
+      4,
+      4,
+      3
+    ]
+},
+{
+  "name":"George Clooney",
+  "photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXMiwyM7Jhu-botPXlc04zi-6gIMxg8v-we1A14eBCe9g460E_",
+  "scores":[
+      2,
+      3,
+      4,
+      5,
+      1,
+      3,
+      2,
+      5,
+      1,
+      2
+    ]
+},
+{
+  "name":"Morgan Freeman",
+  "photo":"https://images.fandango.com/ImageRenderer/0/0/redesign/static/img/default_poster.png/0/images/masterrepository/other/185343652.jpg",
+  "scores":[
+      4,
+      2,
+      3,
+      1,
+      4,
+      3,
+      2,
+      5,
+      1,
+      2
+    ]
+}
+];
+
+module.exports = friends;
+
+
---
+++ server.js
@@ -0,0 +1,18 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const path = require("path");
+const app = express();
+const PORT = 3000;
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+require("./app/routing/htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app);
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+})
